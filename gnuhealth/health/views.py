b43039bd8e6eee7cf3059207c5691c1e18e53a5f
LENOVO <LENOVO@DESKTOP-PSIG1A6>
2019-03-14 14:41:16 +0500

    Urooba

--- gnuhealth/health/views.py
+++ gnuhealth/health/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. 12233")
+    return HttpResponse("Hello, world. 122636322773")
