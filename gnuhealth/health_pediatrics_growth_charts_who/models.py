1d073fe8ba3f510f521a194e962f1bf158eed0c3
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-22 01:22:24 +0500

    Models created for health_pediatrics_growth_charts_who

--- gnuhealth/health_pediatrics_growth_charts_who/models.py
+++ gnuhealth/health_pediatrics_growth_charts_who/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class gnuhealth_pediatrics_growth_charts_who(models.Model):
+  id = models.IntegerField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  indicator = models.CharField()
+  measure = models.CharField()
+  month = models.CharField()
+  sex = models.CharField()
+  type = models.CharField()
+  value = models.CharField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
