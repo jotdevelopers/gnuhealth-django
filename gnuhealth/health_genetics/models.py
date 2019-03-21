6ca7b172136a7d8aac5593cc4a62b2e82e0ae43a
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-21 22:26:18 +0500

    Models created for health_genetics

--- gnuhealth/health_genetics/models.py
+++ gnuhealth/health_genetics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class gnuhealth_gene_variant(models.Model):
+  id = models.IntegerField()
+  aa_change = models.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  name = models.CharField()
+  variant = models.CharField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
