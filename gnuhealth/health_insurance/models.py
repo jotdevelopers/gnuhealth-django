278eb8570aafcb1377ba5731c012d9f09d62331a
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-22 00:00:39 +0500

    Models created for health_insurance

--- gnuhealth/health_insurance/models.py
+++ gnuhealth/health_insurance/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class gnuhealth_insurance_plan(models.Model):
+  id = models.IntegerField()
+  company = models.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  is_default = models.BooleanField()
+  name = models.CharField()
+  notes = models.CharField()
+  write_date = models.CharField()
+  write_uid = models.IntegerField()
+  
+class gnuhealth_insurance_plan_product_policy(models.Model):
+  id = models.IntegerField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  discount = models.IntegerField()
+  plan = models.CharField()
+  product = models.CharField()
+  product_category = models.CharField()
+  write_date = models.CharField()
+  write_uid = models.IntegerField()
