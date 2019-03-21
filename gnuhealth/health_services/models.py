c2ecb68c2a14d7cb76b8c4ca5a81e00c77a63e80
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-21 22:40:33 +0500

    Models created for health_services

--- gnuhealth/health_services/models.py
+++ gnuhealth/health_services/models.py
@@ -18,3 +18,20 @@ class gnuhealth_health_service(models.Model):
   write_uid = models.IntegerField()
   insurance_holder = models.CharField()
   insurance_plan = models.CharField()
+  
+  
+class gnuhealth_health_service_line(models.Model):
+  id = models.IntegerField()
+  appointment = models.CharField()
+  company = models.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  desc = models.CharField()
+  from_date = models.DateField()
+  name = models.CharField()
+  product = models.CharField()
+  qty = models.IntegerField()
+  to_date = models.DateField()
+  to_invoice = models.IntegerField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
