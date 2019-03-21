526c5af4f6b10fb2dbcbdf2e132f0704b05c75a4
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-21 22:49:06 +0500

    Models created for health_iss

--- gnuhealth/health_iss/models.py
+++ gnuhealth/health_iss/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class gnuhealth_iss(models.Model):
+  id = models.IntegerField()
+  alchohol = models.CharField()
+  code = models.IntegerField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  disposition = models.CharField()
+  drugs = models.CharField()
+  healthcenter = models.CharField()
+  injury_date = models.DateField()
+  injury_details = models.CharField()
+  injury_method = models.CharField()
+  injury_type = models.CharField()
+  latitude = models.CharField()
+  longitude = models.CharField()
+  mva_counterpart = models.CharField()
+  mva_mode = models.CharField()
+  mva_position = models.CharField()
+  name = models.CharField()
+  operational_sector = models.CharField()
+  pace_occurence = models.IntegerField()
+  recreation_date = models.DateField()
