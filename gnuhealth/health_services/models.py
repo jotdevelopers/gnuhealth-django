4f6303d8cf0c494bc684d96f11e8b75cdefb016b
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-21 22:33:08 +0500

    Models created for health_services

--- gnuhealth/health_services/models.py
+++ gnuhealth/health_services/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class gnuhealth_health_service(models.Model):
+  id = models.IntegerField()
+  company = models.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  desc = models.CharField()
+  institution = models.CharField()
+  invoice_to = models.CharField()
+  name = models.CharField()
+  patient = models.CharField()
+  service_date = models.DateField()
+  state = models.CharField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
+  insurance_holder = models.CharField()
+  insurance_plan = models.CharField()
