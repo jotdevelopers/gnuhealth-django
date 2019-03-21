ca6ce1bda91243b4f5878cfbe52dc44cb49ee261
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-21 02:48:49 -0700

    Models created for health_paper_archive

--- gnuhealth/health_archives/models.py
+++ gnuhealth/health_archives/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class gnuhealth_paper_archive(models.Model):
+  id = models.IntegerField()
+  comments = models.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  create_location = models.CharField()
+  hc_status = models.BooleanField()
+  legacy = models.CharField()
+  location = models.CharField()
+  patient = models.CharField()
+  request_date = models.DateField()
+  requested_by = models.CharField()
+  return_date = models.DateField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
