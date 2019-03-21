4d12652faf6bc84eb47d5f26be6bf6d4fd52f1eb
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-22 01:45:31 +0500

    Models created for health_company

--- gnuhealth/health_company/models.py
+++ gnuhealth/health_company/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class company_company(models.Model):
+  id = models.IntegerField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  currency = models.IntegerField()
+  footer = models.CharField()
+  header = models.CharField()
+  parent = models.CharField()
+  party = models.CharField()
+  timezone = models.CharField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
+  
+  
+class company_employee(models.Model):
+  id = models.IntegerField()
+  company = models.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  end_date = models.DateField()
+  party = models.CharField()
+  start_date = models.DateField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
+  
