953d4fff13116e704fc62e6282c0b5f679c0bfc9
Fazal Ur Rahman <48551680+frahman30@users.noreply.github.com>
2019-03-22 01:01:58 +0500

    Models created for health_ophthalmology

--- gnuhealth/health_ophthalmology/models.py
+++ gnuhealth/health_ophthalmology/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class gnuhealth_ophthalmology_findings(models.Model):
+  id = models.IntegerField()
+  affected_eye = model.CharField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  eye_structure = mdoels.CharField()
+  finding = models.CharField()
+  name = models.CharField()
+  write_date = models.DateField()
+  write_uid = models.IntegerField()
+  
+class gnuhealth_ophthalmology_evaluation(models.Model):
+  id = models.IntegerField()
+  create_date = models.DateField()
+  create_uid = models.IntegerField()
+  health_professional = models.CharField()
+  iop_method = models.CharField()
+  laxis = models.CharField()
+  lbcva = models.CharField()
+  lbcva_axis = models.CharField()
+  lbcva_cylinder = models.CharField()
+  lbcva_nv = models.CharField()
+  lbcva_nv_add = models.CharField()
+  lbcva_spherical = models.CharField()
+  lcylinder = models.CharField()
+  ldva = models.CharField()
+  ldva_aid = models.CharField()
+  ldva_pinhole = models.CharField()
+  liop = models.CharField()
+  lnv = models.CharField()
+  lnv_add = models.CharField()
+  lspherical = models.CharField()
+  
