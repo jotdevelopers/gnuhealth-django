c4b8c269c35caeb3ea41bc2089c8933f0808d653
LENOVO <LENOVO@DESKTOP-PSIG1A6>
2019-03-22 15:53:22 +0500

    Defined models for health_surgery

--- gnuhealth/health_surgery/models.py
+++ gnuhealth/health_surgery/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-from winpty.tests.test_cywinpty import agent_fixture
-from IPython.core.release import description
-from django.db.migrations import state
 
 # Create your models here.
 
 class gnuhealth_surgery(models.Model):
-<<<<<<< HEAD
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     admission = models.IntegerField()
     age = models.CharField()
     anesthesia_report = models.TextField()
@@ -42,7 +38,7 @@ class gnuhealth_surgery(models.Model):
     main_procedure = models.IntegerField()
     
 class gnuhealth_surgery_supply(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     create_date = models.DateField()
     create_uid = models.IntegerField()
     name = models.IntegerField()
@@ -54,7 +50,7 @@ class gnuhealth_surgery_supply(models.Model):
     write_uid = models.IntegerField()
     
 class gnuhealth_surgery_team(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     create_date = models.DateField()
     create_uid = models.IntegerField()
     name = models.IntegerField()
@@ -69,68 +65,3 @@ class Meta:
     db_table = 'gnuhealth_surgery'
     db_table = 'gnuhealth_surgery_supply'
     db_table = 'gnuhealth_surgery_team'
-=======
-  id = models.IntegerField()
-  admission = models.CharField()
-  age = models.IntegerField()
-  anesthesia_report = models.CharField()
-  anesthetist = models.CharField()
-  classification = models.CharField()
-  code = models.IntegerField()
-  create_date = models.DateField()
-  create_uid = models.IntegerField()
-  description = models.CharField()
-  extra_info = models.CharField()
-  institution = models.CharField()
-  operating_room = models.CharField()
-  pathology = models.CharField()
-  patient = models.CharField()
-  postoperative_dx = models.CharField()
-  preop_antibiotics = models.CharField()
-  preop_asa = models.CharField()
-  preop_bleeding_risk = models.CharField()
-  preop_mallampati = models.CharField()
-  preop_oximeter = models.CharField()
-  preop_rci = models.CharField()
-  preop_site_marketing = models.CharField()
-  preop_sterility = models.CharField()
-  signed_by = models.CharField()
-  state = models.CharField()
-  surgeon = models.CharField()
-  surgeon_date = models.DateField()
-  surgery_end_date = models.DateField()
-  surgical_wound = models.CharField()
-  write_date = models.DateField()
-  write_uid = models.IntegerField()
-  main_procedure = models.CharField()
-  
-class gnuhealth_surgery_team(models.Model):
-  id = models.IntegerField()
-  create_date = models.DateField()
-  create_uid = models.IntegerField()
-  name = models.CharField()
-  notes = models.CharField()
-  role = models.CharField()
-  team_member = models.CharField()
-  write_date = models.DateField()
-  write_uid = models.IntegerField()
-  
-
-class gnuhealth_surgery_supply(models.Model):
-  id = models.IntegerField()
-  create_date = models.DateField()
-  create_uid = models.IntegerField()
-  name = models.CharField()
-  notes = models.CharField()
-  qty = models.IntegerField()
-  qty_used = models.IntegerField()
-  supply = models.CharField()
-  write_date = models.DateField()
-  write_uid = models.IntegerField()
-  
-
-
-
-
-
->>>>>>> branch 'master' of https://github.com/jotdevelopers/gnuhealth-django.git
