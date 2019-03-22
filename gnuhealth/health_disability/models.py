ff56d40ac25857f0cc53f12ee3306fb6d4306d0f
LENOVO <LENOVO@DESKTOP-PSIG1A6>
2019-03-22 17:45:00 +0500

    added meta in health_disability, defined models for
    health_disability

--- gnuhealth/health_disability/models.py
+++ gnuhealth/health_disability/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+ class gnuhealth_patient_disability_assessment(models.Model):
+    id = models.IntegerField(primary_key=True)
+    activity_participation = models.CharField()
+    assessment = models.CharField()
+    assessment_date = models.DateField()
+    cognitive_function = models.CharField()
+    create_date = models.DateTimeField()
+    create_uid = models.IntegerField()
+    crutches = models.BooleanField()
+    hand_function = models.CharField()
+    healthprof = models.IntegerField()
+    hearing_function = models.CharField()
+    locomotor_function = models.CharField()
+    notes = models.TextField()
+    patient = models.IntegerField()
+    speech_function = models.CharField()
+    visual_function = models.CharField()
+    wheelchair = models.BooleanField()
+    write_date = models.DateTimeField()
+    write_uid = models.IntegerField()
+    
+class Meta:
+    db_table = 'gnuhealth_patient_disability_assessment'
