f2d7f6cdd73802ccaa92142c826e324e232387f4
LENOVO <LENOVO@DESKTOP-PSIG1A6>
2019-03-22 17:25:06 +0500

    added meta class in health_country

--- gnuhealth/health_country/models.py
+++ gnuhealth/health_country/models.py
@@ -3,35 +3,40 @@ from django.db import models
 # Create your models here.
 
 class country_subdivision(models.Model):
-  id = models.IntegerField()
+  id = models.IntegerField(primary_key=True)
   code = models.IntegerField()
   country = models.CharField()
-  create_date = models.DateField()
+  create_date = models.DateTimeField()
   create_uid = models.IntegerField()
   name =  models.CharField()
   parent = models.CharField()
   type = models.CharField()
-  write_date = models.DateField()
+  write_date = models.DateTimeField()
   write_uid = models.IntegerField()
   
 class country_zip(models.Model):
-  id = models.IntegerField()
+  id = models.IntegerField(primary_key=True)
   city = models.CharField()
   country = models.CharField()
-  create_date = models.DateField()
+  create_date = models.DateTimeField()
   create_uid = models.IntegerField()
   subdivision =  models.CharField()
-  write_date = models.DateField()
+  write_date = models.DateTimeField()
   write_uid = models.IntegerField()
   zip = models.IntegerField()
   
 class country_country(models.Model):
-  id = models.IntegerField()
+  id = models.IntegerField(primary_key=True)
   code = models.IntegerField()
   code3 = models.IntegerField()
   code_numeric = models.IntegerField()
-  create_date = models.DateField()
+  create_date = models.DateTimeField()
   create_uid = models.IntegerField()
   name =  models.CharField()
-  write_date = models.DateField()
+  write_date = models.DateTimeField()
   write_uid = models.IntegerField()
+
+class Meta:
+    db_table ='country_subdivision'
+    db_table = 'country_zip'
+    db_table = 'country_country'
