b2a76862944399d7234d05664baf167e510be866
LENOVO <LENOVO@DESKTOP-PSIG1A6>
2019-03-22 13:58:25 +0500

    Defined models for health_genetics from tables
    gnuhealth_gene_variant & gnuhealth_gene_variant_phenotype

--- gnuhealth/health_genetics/models.py
+++ gnuhealth/health_genetics/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class gnuhealth_gene_variant(models.Model):
+    id = models.IntegerField()
+    aa_change = models.CharField()
+    create_date = models.DateTimeField()
+    create_uid = models.IntegerField()
+    name = models.IntegerField()
+    varient = models.CharField()
+    write_date = models.DateTimeField()
+    write_uid = models.IntegerField()
+    
+class gnuhealth_gene_variant_phenotype(models.Model):
+    id = models.IntegerField()
+    create_date = models.DateTimeField()
+    create_uid = models.IntegerField()
+    name = models.IntegerField()
+    phenotype = models.IntegerField()
+    varient = models.CharField()
+    write_date = models.DateTimeField()
+    write_uid = models.IntegerField()
+    
+class Meta:
+    db_table ='gnuhealth_gene_variant'
+    db_table ='gnuhealth_gene_variant_phenotype'
