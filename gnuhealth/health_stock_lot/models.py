412f3399258e324ac27fa2713164f30dd0a0fd9e
LENOVO <LENOVO@DESKTOP-PSIG1A6>
2019-03-30 16:02:47 +0500

    defined models for health_stock & health_stock_lot
    db_tables for health_stock:
    'stock_configuration'
    'stock_configuration_location'
    'stock_configuration_sequence'
    'stock_inventory'
    'stock_inventory_line'
    'stock_location'
    'stock_location_lead_time'
    'stock_move'
    'stock_order_point'
    'stock_period'
    'stock_period_cache'
    'stock_period_cache_lot'
    'stock_shipment_in'
    'stock_shipment_in_return'
    'stock_shipment_internal'
    'stock_shipment_out'
    'stock_shipment_out_return'
    
    db_tables for health_stock_lot:
    'stock_lot'
    'stock_lot_type'

--- gnuhealth/health_stock_lot/models.py
+++ gnuhealth/health_stock_lot/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class stock_lot(models.Model):
+    id = models.IntegerField(primary_key=True)
+    create_date = models.DateTimeField(max_length=6)
+    create_uid = models.IntegerField()
+    number = models.CharField()
+    product = models.IntegerField()
+    write_date = models.DateTimeField(max_length=6)
+    write_uid = models.IntegerField()
+    expiration_date = models.DateField()
+    
+class stock_lot_type(models.Model):
+    id = models.IntegerField(primary_key=True)
+    code = models.CharField()
+    create_date = models.DateTimeField(max_length=6)
+    create_uid = models.IntegerField()
+    name = models.CharField()
+    write_date = models.DateTimeField(max_length=6)
+    write_uid = models.IntegerField()
+
+class Meta:
+    db_table = 'stock_lot'
+    db_table = 'stock_lot_type'
