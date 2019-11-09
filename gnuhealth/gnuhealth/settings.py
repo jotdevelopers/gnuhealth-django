92b72f572f090886d8a380cd36ef0a8bce7d8f70
frahman30 <frahman.iesl@gmail.com>
2019-11-09 16:43:42 +0500

    Completed the templates work of our modules

--- gnuhealth/gnuhealth/settings.py
+++ gnuhealth/gnuhealth/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'gnuhealth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testdb',
-        'USER': 'gnuhealth',
-        'PASSWORD': 'password6',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'testdb',
+#         'USER': 'gnuhealth',
+#         'PASSWORD': 'password6',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
