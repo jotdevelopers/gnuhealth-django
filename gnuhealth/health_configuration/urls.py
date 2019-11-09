038e70cff8b95bfe6657963de521c996019e9105
ScriptKiddie <jotdevelopers@gmail.com>
2019-11-09 22:33:48 +0500

    Lookups Integrated.

--- gnuhealth/health_configuration/urls.py
+++ gnuhealth/health_configuration/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('condition', views.condition, name='condition'),
+    path('pathologyGroups', views.pathologyGroups, name='pathologyGroups'),
 ]
--- gnuhealth/health_configuration/views.py
+++ gnuhealth/health_configuration/views.py
@@ -1,7 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from health.models import gnuhealth_pathology
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world.")
+
+# Create Conditions Views
+def condition(request):
+    conditions = gnuhealth_pathology.objects.all()
+    return render(request, 'health_configuration/conditions/conditions.html', {'conditions': conditions})
+#
+def pathologyGroups(request):
+    return render(request, 'health_configuration/conditions/pathology_groups.html')
+#
+#
+# def diseaseCategories(request):
+#
+#
+# # Create Genetics Views
+# def diseaseGenes(request):
+#
+#
+# def naturalVariants(request):
+#
+#
+# def variantPhenotypes(request):
+#
+#
+# def proteinRelatedDiseases(request):
+#
+#
+# # Create DX Imaging  Views
+# def dxImagingTestTypes(request):
+#
+#
+# def dxImagingTests(request):
+#
+#
+# # Create Functioning and Disability  Views
+# def bodyFunctions(request):
+#
+#
+# def bodyStructures(request):
+#
+#
+# # Create Procedure  Views
+# def procedures(request):
+#
+#
+# # Create Institutions Views
+# def buildings(request):
+#
+#
+# # Create DX Imaging  Views
+# def variantPhenotypes(request):
+#
+#
+# def variantPhenotypes(request):
