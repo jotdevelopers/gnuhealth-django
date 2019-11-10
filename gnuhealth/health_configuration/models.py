61c469e3a18dcd74266cbe968ab9f7899b9ba8e3
ScriptKiddie <jotdevelopers@gmail.com>
2019-11-10 16:35:43 +0500

    Layouts Updated

--- gnuhealth/health_configuration/models.py
+++ gnuhealth/health_configuration/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class gnuhealth_pathology(models.Model):
-    id = models.IntegerField(primary_key=True)
-    create_date = models.DateTimeField()
-    write_date = models.DateTimeField()
-    create_uid = models.IntegerField()
-    write_uid = models.IntegerField()
-    category = models.IntegerField()
-    code = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    info = models.CharField(max_length=100)
-    protein = models.CharField(max_length=100)
-    gene = models.CharField(max_length=100)
-    chromosome = models.CharField(max_length=100)
-    active = models.BooleanField()
-
-    class Meta:
-        db_table = 'gnuhealth_pathology'
--- gnuhealth/health_configuration/urls.py
+++ gnuhealth/health_configuration/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('citienship', views.citizenship, name='citienship'),
     path('add_citienship', views.add_citizenship, name='add_citienship'),
     path('occupation', views.occupation, name='occupation'),
-    path('add_occupation', views.add_occupationp, name='add_occupation'),
+    path('add_occupation', views.add_occupation, name='add_occupation'),
     path('residence', views.residence, name='residence'),
     path('add_residence', views.add_residence, name='add_residence'),
 ]
