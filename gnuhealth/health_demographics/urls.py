4099f385fcc34b6566c83277b35362a7ebd52341
ScriptKiddie <jotdevelopers@gmail.com>
2019-11-21 12:55:43 +0500

    General Changes

--- gnuhealth/health_demographics/urls.py
+++ gnuhealth/health_demographics/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('birthCertificates', views.birthCertificates, name='birthCertificates'),
-    path('addBirthCertificate', views.addBirthCertificate, name='addBirthCertificate'),
-    path('editBirthCertificate/<id>', views.editBirthCertificate, name='editBirthCertificate'),
-    path('updateBirthCertificate/<id>/', views.updateBirthCertificate, name='updateBirthCertificate'),
-    path('deleteBirthCertificate/<id>/', views.deleteBirthCertificate, name='deleteBirthCertificate'),
+    #path('birthCertificates', views.birthCertificates, name='birthCertificates'),
+    #path('addBirthCertificate', views.addBirthCertificate, name='addBirthCertificate'),
+    #path('editBirthCertificate/<id>', views.editBirthCertificate, name='editBirthCertificate'),
+    #path('updateBirthCertificate/<id>/', views.updateBirthCertificate, name='updateBirthCertificate'),
+    #path('deleteBirthCertificate/<id>/', views.deleteBirthCertificate, name='deleteBirthCertificate'),
     
-     path('', views.index, name='index'),
-    path('deathCertificates', views.deathCertificates, name='deathCertificates'),
-    path('addDeathCertificate', views.addDeathCertificate, name='addDeathCertificate'),
-    path('editDeathCertificate/<id>', views.editDeathCertificate, name='editDeathCertificate'),
-    path('updateDeathCertificate/<id>/', views.updateDeathCertificate, name='updateDeathCertificate'),
-    path('deleteDeathCertificate/<id>/', views.deleteDeathCertificate, name='deleteDeathCertificate'),
+    path('', views.index, name='index'),
+    #path('deathCertificates', views.deathCertificates, name='deathCertificates'),
+    #path('addDeathCertificate', views.addDeathCertificate, name='addDeathCertificate'),
+    #path('editDeathCertificate/<id>', views.editDeathCertificate, name='editDeathCertificate'),
+    #path('updateDeathCertificate/<id>/', views.updateDeathCertificate, name='updateDeathCertificate'),
+    #path('deleteDeathCertificate/<id>/', views.deleteDeathCertificate, name='deleteDeathCertificate'),
 ]
