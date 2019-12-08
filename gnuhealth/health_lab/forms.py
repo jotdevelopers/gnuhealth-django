59c08cea6f985c8a589e5e6820f1687f2709754a
frahman30 <frahman.iesl@gmail.com>
2019-12-08 15:59:20 +0500

    Created lab tests module

--- gnuhealth/health_lab/forms.py
+++ gnuhealth/health_lab/forms.py
@@ -1,6 +1,22 @@
-from django import forms  
-from health_lab.models import gnuhealth_lab  
-class HealthLabForm(forms.ModelForm):  
-    class Meta:  
-        model = gnuhealth_lab   
-        fields = ['_all_'] 
+from django import forms
+from django.forms import ModelMultipleChoiceField
+from django_select2.forms import Select2MultipleWidget
+from health_lab.models import *
+
+
+# class HealthLabForm(forms.ModelForm):  
+#     class Meta:  
+#         model = gnuhealth_lab   
+#         fields = '__all__'
+
+
+class labTestUnitsForm(forms.ModelForm):
+    class Meta:
+        model = gnuhealth_lab_test_units
+        fields = ['id','code','name','create_date', 'write_date']
+
+
+class labTestTypeForm(forms.ModelForm):
+    class Meta:
+        model = gnuhealth_lab_test_type
+        fields = ['id','active','code','info','name','product_id','create_date', 'write_date']
